150eea4c25ef8f0b84a84674a64dae7fc6ca03f4
michalMenda <m0583216525@gmail.com>
2025-06-18 13:01:36 +0300

    MainLayout

--- swim-app/client/components/MainLayout.jsx
+++ swim-app/client/components/MainLayout.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Outlet, useParams, useLocation } from 'react-router-dom';
+import { userContext } from './App';
 import NavBar from './NavBar';
+import NotFound from './NotFound';
 
 export default function MainLayout() {
+  const { username } = useParams();
+  const { userData } = useContext(userContext);
+  const location = useLocation();
+  const isProtectedRoute = username && location.pathname.includes(username);
+  
+  if (isProtectedRoute && (!userData || username !== userData.username)) {
+    return (
+      <>
+        <NavBar />
+        <main>
+          <NotFound />
+        </main>
+      </>
+    );
+  }
+
   return (
     <>
       <NavBar />
